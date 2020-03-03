0a0e133e4f5e5921cb6d3b5ab675073c8395fd38
Guillem Ramírez Miranda <guillem.ramirez@ieee.org>
2020-03-03 12:09:23 +0100

    Added base taskloop support

--- task.go
+++ task.go
@@ -1,6 +1,7 @@
 package miniomp
 
 type Fn func([]interface{}) interface{}
+type LoopFn func(int, []interface{})
 
 type Task struct {
 	f Fn
@@ -21,4 +22,31 @@ func NewTask(f Fn, params []interface{}){
 		f: f,
 		params: params,
 	}
+}
+
+func task4TaskLoop(args []interface{}) interface{}{
+	fun := args[0].(LoopFn)
+	init := args[1].(int)
+	fi := args[2].(int)
+	loopFnArgs := args[3].([]interface{})
+
+	for i:=init; i < fi; i++ {
+		fun(i, loopFnArgs)
+	}
+
+	return nil
+}
+
+func TaskLoop(f LoopFn, iterations int, loopFnArgs []interface{}){
+
+	var BS int = (iterations/miniomp_nt)/2
+	for i:=0; i < iterations; i += BS {
+		fin :=  i+BS
+		if  fin >= iterations {
+			fin = iterations
+		}
+		NewTask(task4TaskLoop, []interface{}{ f, i, fin, loopFnArgs})
+	}
+
+	TaskWait()
 }
---
+++ utils.go
@@ -0,0 +1,10 @@
+package miniomp
+
+func IntSliceToInterface( a []int ) []interface{} {
+
+	var interfaceSlice []interface{} = make([]interface{}, len(a))
+	for i, d := range a {
+		interfaceSlice[i] = d
+	}
+	return interfaceSlice
+}
