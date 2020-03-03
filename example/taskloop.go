ad567fd28e34b3b8c9af25060f67431edaaa7aad
Guillem Ramírez Miranda <guillem.ramirez@ieee.org>
2020-03-03 12:14:06 +0100

    Updated taskloop example to show how to pass multiple arrays to taskloop

--- example/taskloop.go
+++ example/taskloop.go
@@ -5,14 +5,17 @@ import "miniomp"
 import "fmt"
 
 func calcIt( i int, data []interface{} ){
-		fmt.Print("Value is ", data[i].(int)+data[i].(int), "\n")
+		a  := data[0].([]interface{})
+		b  := data[1].([]interface{})
+		fmt.Print("Value is ", a[i].(int)+b[i].(int), "\n")
 }
 
 func master_func(){
 
 	A := []int{ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18 }
+	B := []int{ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18 }
 
-	miniomp.TaskLoop(calcIt, len(A), miniomp.IntSliceToInterface(A) )
+	miniomp.TaskLoop(calcIt, len(A), []interface{} { miniomp.IntSliceToInterface(A), miniomp.IntSliceToInterface(B) } )
 
 }
 
