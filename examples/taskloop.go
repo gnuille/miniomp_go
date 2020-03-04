feb2ed9574fb56de29c869462d966d0aff03ffd1
Guillem Ramírez Miranda <guillem.ramirez@ieee.org>
2020-03-04 13:24:49 +0100

    Fixed taskloop bug when BS == 0 infinite loop is produced, updated taskloop example for high computational cost, need to fix data gen in order to speed up the generation and focus more on the computation for a better trace study

--- examples/taskloop.go
+++ examples/taskloop.go
@@ -8,9 +8,16 @@ import "math/rand"
 var count int64
 
 func calcIt( i int, data []interface{} ){
-	a  := data[0].([]interface{})
-	b  := data[1].([]interface{})
-	fmt.Println(a[i].(int)+b[i].(int))
+
+	a  := data[0].([]interface{})[i].([]interface{})
+	b  := data[1].([]interface{})[i].([]interface{})
+
+	var res int = 0
+	for it,_ := range a {
+		res += a[it].(int) * b[it].(int)
+	}
+
+	fmt.Println(res)
 }
 
 func randomArray(len int) []int {
@@ -21,13 +28,21 @@ func randomArray(len int) []int {
     return a
 }
 
+func randomArrayOfArray(len int) [][]int {
+	a := make([][]int, len)
+	for i := 0; i < len; i++ {
+		a[i] = randomArray(len)
+	}
+	return a
+}
+
 func master_func(){
 
 
-	A := randomArray(100000)
-	B := randomArray(100000)
+	A := randomArrayOfArray(10000)
+	B := randomArrayOfArray(10000)
 
-	miniomp.TaskLoop(calcIt, len(A), []interface{} { miniomp.IntSliceToInterface(A), miniomp.IntSliceToInterface(B) } )
+	miniomp.TaskLoop(calcIt, len(A), []interface{} { miniomp.IntSliceIntSliceToInterface(A), miniomp.IntSliceIntSliceToInterface(B) } )
 
 }
 
